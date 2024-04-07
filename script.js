b29028fe3f84478f6dcd3571827a855c830e77a6
Chiến <133650540+hoangchien204@users.noreply.github.com>
2024-04-07 11:01:14 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// script.js
+function openLoginForm() {
+    document.getElementById("loginForm").style.display = "block";
+  }
+  
+  function closeLoginForm() {
+    document.getElementById("loginForm").style.display = "none";
+  }
+//   document.getElementById('category').addEventListener('click', function() {
+//     var submenu = document.getElementById('category-menu');
+//     submenu.classList.toggle('active');
+// });
+
+// // Tương tự, bạn có thể làm cho phần thương hiệu
+// document.getElementById('brand').addEventListener('click', function() {
+//     var submenu = document.getElementById('brand-menu');
+//     submenu.classList.toggle('active');
+// });
+
+  
